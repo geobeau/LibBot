d53e76c2332f08a5ea04fabf0124fb5fe4d4ec4e
Geoffrey Beausire <g.beausire@criteo.com>
2019-07-17 23:21:55 +0200

    Add download capabilities

--- cmd/libbot.go
+++ cmd/libbot.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"strings"
 	"fmt"
+	"mime"
+	"regexp"
 	"github.com/PuerkitoBio/goquery"
 	"net/http"
 
@@ -17,16 +19,18 @@ type book struct {
 	author string
 	title string
 	year string
+	checksum string
 }
 
 func extractBooks(resp http.Response) []book {
+	r, _ := regexp.Compile("md5=([a-zA-Z0-9]+)")
+
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 	  log.Fatal(err)
 	}
 	books := []book {}
 	doc.Find("table.c tr:not(:first-child)").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
 		row := s.Find("td")
 		id := row.Eq(0).Text()
 		author := row.Eq(1).Text()
@@ -34,12 +38,49 @@ func extractBooks(resp http.Response) []book {
 		for i := range selector.Nodes {
 			selector.Eq(i).Remove()
 		}
+		infoURL := row.Eq(2).Find("a[title]").Eq(0).AttrOr("href", "")
+		// Todo: make it more robust
+		checksum := r.FindStringSubmatch(infoURL)[1]
 		title := row.Eq(2).Find("a[title]").Eq(0).Text()
 		year := row.Eq(4).Text()
-		books = append(books, book{id, author, title, year})
+		books = append(books, book{id, author, title, year, checksum})
 	})
 	return books
 }
+
+func extractDownloadURL(resp http.Response) string {
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	downloadURL := ""
+	log.Println("Searching")
+	doc.Find("#info a").Each(func(i int, s *goquery.Selection) {
+		if s.Text() == "GET" {
+			downloadURL = s.AttrOr("href", "")
+		}
+	})
+	return downloadURL
+}
+
+func getBookFile(checksum string) (*http.Response, error) {
+	apiBaseURL := "http://93.174.95.29"
+	apiURL := fmt.Sprintf(apiBaseURL + "/_ads/%s", checksum)
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		log.Println("Failed to query URL: ", apiURL)
+		return nil, err
+	}
+	downloadURL := apiBaseURL + extractDownloadURL(*resp)
+	log.Println("Downloading: ", downloadURL)
+	resp, err = http.Get(downloadURL)
+	if err != nil {
+		log.Println("Failed to query URL: ", apiURL)
+		return nil, err
+	}
+	return resp, nil
+}
+
 
 func formatBookMessage(book book) string {
 	template := 
@@ -83,6 +124,22 @@ func main() {
 	}
 	log.Println("Connected to api")
 
+	downloadButton := tb.InlineButton{
+		Unique: "download_button",
+		Text: "Download",
+	}
+
+	b.Handle(&downloadButton, func(c *tb.Callback) {
+		b.Respond(c, &tb.CallbackResponse{Text: "Downloading..."})
+		bookResp, _ := getBookFile(c.Data)
+		_, params, _ := mime.ParseMediaType(bookResp.Header.Get("Content-Disposition"))
+		telegramFile := tb.FromReader(bookResp.Body)
+		telegramFile.FileName = params["filename"]
+		bookFile := &tb.Document{File: telegramFile}
+		log.Println("Sending: ", params["filename"])
+		bookFile.Send(b, c.Sender, nil)
+	})
+
 	b.Handle(tb.OnText, func(m *tb.Message) {
 		log.Println("Received:", m.Text)
 		query := m.Text
@@ -94,14 +151,19 @@ func main() {
 		}
 		for i := range books {
 			log.Println(books[i])
-			b.Send(m.Sender, formatBookMessage(books[i]), tb.ModeMarkdown)
+			downloadButton.Data = books[i].checksum
+			inlineButtons := [][]tb.InlineButton{
+				[]tb.InlineButton{downloadButton},
+			}
+			b.Send(m.Sender, formatBookMessage(books[i]), tb.ModeMarkdown, &tb.ReplyMarkup{
+				InlineKeyboard: inlineButtons,
+			})
 			if i >= 10 {
 				break
 			}
-		}
-
-		
+		}		
 	})
+
 	log.Println("Handler started")
 	b.Start()
 }
